275cbb3305bc5ee5f0de14cc5e46b8c9688d81ff
Jorge Anais <jrganais@gmail.com>
2023-03-31 21:40:53 -0300

    Added proposed solution notebook feedforward

---
+++ notebooks/01_perceptron_Rosenblatt.py
@@ -0,0 +1,18 @@
+Sonificación de imágenes astronómicas
+
+El telescopio espacial James Webb en su primer año de vida a revolucionado la astronomía moderna. Las calidad y profundidad de las imágenes obtenidas con este telescopio no tienen precedente, y han puesto en jaque desde las teorías actuales de la formación de nuestro universo, hasta la posibilidad de estudiar rastros de vida en otros planetas. Las imágenes tomadas por el telescopio espacial son de uso público y pueden encontrarse aquí: https://webb.nasa.gov/content/multimedia/images.html
+
+La sonificación es el proceso de transformación desde una estructura de datos propias de una imágen hacia la modificación de los parámetros de un sonido.
+
+Este proyecto pretende utilizar la sonificación de imágenes como una herramienta de inclusión para la difusión de este contenido para personas en situación de discapacidad visual o con baja visión. 
+
+Para esto, se extraerán características numéricas de una imágen astronómica mediante algoritmos de aprendizaje de máquinas y/o visión por computadora de una región seleccionada dentro de la imágen. Estas características permitiran modificar el sonido a medida que se barre la región de la imágen.
+
+¿Que se requiere hacer en este proyecto?
+
+1. Implementar de una herramienta que permita extraer características de la imágen utilizando algoritmos de aprendizaje de máquinas y/o visión por computadora.
+2. Realizar una síntesis de sonido con las características numéricas extraidas de una región. 
+3. Crear una interfaz de usuario para que el humano interactúe con la imágen.
+
+Software.
+
